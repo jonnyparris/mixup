30cc5330b925542f1b4d905c263878626cba0f85
Ruskin Constant <ruskin.constant+dev@gmail.com>
2014-11-23 00:43:38 +0100

    tracks have stem and remix submissions

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,5 +1,6 @@
 class Track < ActiveRecord::Base
   belongs_to :creator, class_name: "User"
 
-  has_many :submissions, foreign_key: "original_id"
+  has_many :stem_submissions, class_name: "Submission", foreign_key: "original_id"
+  has_many :remix_submissions, class_name: "Submission", foreign_key: "remix_id"
 end
--- spec/models/tracks_spec.rb
+++ spec/models/tracks_spec.rb
@@ -9,7 +9,8 @@ RSpec.describe Track, :type => :model do
 
   describe "associations" do
     it { should belong_to(:creator).class_name('User') }
-    it { should have_many(:submissions).with_foreign_key('original_id') }
+    it { should have_many(:stem_submissions).class_name('Submission').with_foreign_key('original_id') }
+    it { should have_many(:remix_submissions).class_name('Submission').with_foreign_key('remix_id') }
     xit { should have_many(:circles).through(:remixes) }
   end
 end
