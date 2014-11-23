a85f0c0af88bf207999a629c1087d182fbaca6fb
Ruskin Constant <ruskin.constant+dev@gmail.com>
2014-11-23 13:35:20 +0100

    add both association and validation tests for submissions

--- spec/models/submissions_spec.rb
+++ spec/models/submissions_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Submission, :type => :model do
-  it { should respond_to(:original_id) }
-  it { should respond_to(:remix_id) }
-  it { should respond_to(:circle_id) }
+  describe "attributes" do
+    it { should respond_to(:original_id) }
+    it { should respond_to(:remix_id) }
+    it { should respond_to(:circle_id) }
+  end
+
+  describe "associations" do
+    it { should belong_to(:circle) }
+    it { should belong_to(:original).class_name("Track") }
+    it { should belong_to(:remix).class_name("Track") }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:original_id) }
+    it { should validate_presence_of(:circle_id) }
+  end
 end
