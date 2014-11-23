e206c01df5da206c4352f3b856a78d951045cc31
Ruskin Constant <ruskin.constant+dev@gmail.com>
2014-11-23 13:29:35 +0100

    pass circle validation tests

--- app/models/circle.rb
+++ app/models/circle.rb
@@ -2,4 +2,8 @@ class Circle < ActiveRecord::Base
   belongs_to :creator, class_name: "User"
 
   has_many :submissions
+
+  validates_presence_of :name
+  validates_uniqueness_of :name, scope: :creator_id
+  validates :signup_deadline, date: { before: :submit_deadline }
 end
