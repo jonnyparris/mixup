0849006898caed25434c2ff01cea0c03abda2e13
Ruskin Constant <ruskin.constant+dev@gmail.com>
2014-11-23 12:41:57 +0100

    pass user validation tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ActiveRecord::Base
 
   has_many :tracks, foreign_key: "creator_id"
   has_many :circles, foreign_key: "creator_id"
+
+  validates_presence_of :user_name
+  validates_presence_of :email
+
+  validates_length_of :user_name, maximum: 64
+
+  validates_uniqueness_of :user_name, case_sensitive: false
+  validates_uniqueness_of :email, case_sensitive: false
 end
--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe User, :type => :model do
   describe "validations" do
     it { should validate_presence_of(:user_name) }
     it { should validate_presence_of(:email) }
-    it { should validate_length_of(:user_name).is_at_most(64) }
+    it { should ensure_length_of(:user_name).is_at_most(64) }
     it { should have_secure_password }
     it { should validate_uniqueness_of(:email).case_insensitive }
     it { should validate_uniqueness_of(:user_name).case_insensitive }
