f8b1fd4bebe4943603a417b9ee0c7622a609cf16
Ruskin Constant <ruskin.constant+dev@gmail.com>
2014-11-23 12:58:35 +0100

    add track validation tests

--- spec/models/tracks_spec.rb
+++ spec/models/tracks_spec.rb
@@ -11,6 +11,12 @@ RSpec.describe Track, :type => :model do
     it { should belong_to(:creator).class_name('User') }
     it { should have_many(:stem_submissions).class_name('Submission').with_foreign_key('original_id') }
     it { should have_many(:remix_submissions).class_name('Submission').with_foreign_key('remix_id') }
-    xit { should have_many(:circles).through(:remixes) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:track_name) }
+    it { should validate_presence_of(:download_url) }
+    it { should validate_presence_of(:creator_id) }
+    it { should validate_uniqueness_of(:track_name).case_insensitive.scoped_to(:creator_id) }
   end
 end
