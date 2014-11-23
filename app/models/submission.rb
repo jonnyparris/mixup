b527c7201f0e86b98285ee2a0e172f638b601188
Ruskin Constant <ruskin.constant+dev@gmail.com>
2014-11-23 13:36:51 +0100

    pass submission tests

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -2,4 +2,7 @@ class Submission < ActiveRecord::Base
   belongs_to :circle
   belongs_to :original, class_name: "Track"
   belongs_to :remix, class_name: "Track"
+
+  validates_presence_of :original_id
+  validates_presence_of :circle_id
 end
