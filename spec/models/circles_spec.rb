be8f54ca192e9709fe80f1ca8a0990e90116bd42
Ruskin Constant <ruskin.constant+dev@gmail.com>
2014-11-23 13:29:25 +0100

    add circle validation tests

--- spec/models/circles_spec.rb
+++ spec/models/circles_spec.rb
@@ -13,4 +13,14 @@ RSpec.describe Circle, :type => :model do
     it { should belong_to(:creator).class_name("User") }
     it { should have_many(:submissions) }
   end
+
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name).scoped_to(:creator_id) }
+
+    it "is invalid if signup_deadline is after submit_deadline" do
+      invalid_circle = Circle.new(name: 'Xmas Giggles', signup_deadline: '1/12/2014', submit_deadline: '1/11/2014')
+      expect(invalid_circle).to have(1).errors_on(:signup_deadline)
+    end
+  end
 end
