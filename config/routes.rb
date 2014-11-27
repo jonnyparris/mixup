0f40e612fb6c3e1890a2132f5783765ca095213d
Ruskin Constant <ruskin.constant+dev@gmail.com>
2014-11-27 23:36:58 +0100

    create users controller with placeholder dashboard view

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'static_pages#index'
 
+  get 'users/:id/dashboard' => 'users#dashboard'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
