78baf514242229ed943b4bb01d03c6bf4491bbc6
Ruskin Constant <ruskin.constant+dev@gmail.com>
2014-12-08 11:25:27 +0100

    fix failing user dashboard test for no circles edge case

--- spec/features/user_dashboard_feature_spec.rb
+++ spec/features/user_dashboard_feature_spec.rb
@@ -1,6 +1,6 @@
 describe "User Dashboard" do
   it "includes list of all circles by default" do
     visit 'users/1/dashboard'
-    expect(page).to have_content("All Circles")
+    expect(page).to have_content("No Circles")
   end
 end
