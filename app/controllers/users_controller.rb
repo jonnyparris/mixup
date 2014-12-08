46e697e54283049d8a53e82ea16b0c8983175566
Ruskin Constant <ruskin.constant+dev@gmail.com>
2014-12-08 12:44:34 +0100

    pass prev/next circle links test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController<ApplicationController
 
   def dashboard
-    @circles = Circle.includes(:creator).limit(5).offset(params[:circle_offset])
-    @remaining_circles = Circle.count - params[:circle_offset].to_i - 5
+    @total_circles = Circle.count
+    @circle_offset = params[:circle_offset].to_i
+    @circles = Circle.includes(:creator).limit(5).offset(@circle_offset)
+    @remaining_circles = @total_circles - @circle_offset - 5
   end
 end
