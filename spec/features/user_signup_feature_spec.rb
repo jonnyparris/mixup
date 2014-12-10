5d67f30b83a97676a2ed1ca1d25fa2c4333100de
Ruskin Constant <ruskin.constant+dev@gmail.com>
2014-12-10 22:38:21 +0100

    add simple user signup feature tests

---
+++ spec/features/user_signup_feature_spec.rb
@@ -0,0 +1,18 @@
+feature "User Signup" do
+  scenario "should flag an error if it fails" do
+    visit new_user_path
+    click_button("Create User")
+    expect(page).to have_content("Sorry")
+  end
+
+  scenario "should create new user with minimal fields filled in" do
+    User.delete_all
+    visit new_user_path
+    fill_in "User name", with: "j_dilla"
+    fill_in "Email", with: "j_dilla"
+    fill_in "Password digest", with: "j_dilla"
+    fill_in "Password confirmation", with: "j_dilla"
+    click_button("Create User")
+    expect(page).to have_content("Welcome")
+  end
+end
