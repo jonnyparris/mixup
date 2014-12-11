342b8fee003c37cfa3db6bb6193d8d01c88699a6
Ruskin Constant <ruskin.constant+dev@gmail.com>
2014-12-11 12:02:28 +0100

    add create track feature test

---
+++ spec/features/new_tracks_feature_spec.rb
@@ -0,0 +1,18 @@
+feature "Add new tracks" do
+  scenario "should succeed for stems where user is specified" do
+    User.delete_all
+    j_dilla = User.create(first_name: Faker::Name.first_name,
+                          last_name: Faker::Name.last_name,
+                          user_name: Faker::Internet.user_name,
+                          email: Faker::Internet.email,
+                          avatar: Faker::Avatar.image,
+                          location: Faker::Address.city,
+                          password_digest: "pop"
+                          )
+    visit new_user_stem_path(j_dilla.id)
+    fill_in "Download url", with: "https://soundcloud.com/alt-j/sets/breezeblock-remix-stems"
+    fill_in "Track name", with: "BreezeBlock 2012 (original mix"
+    click_button("Submit")
+    expect(page).to have_content("Success!")
+  end
+end
