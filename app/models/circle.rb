562e8c1d921975293d3a6292cfb1c69d25602d2f
jonnyparris <ruskin.constant+dev@gmail.com>
2015-01-16 20:23:53 -0400

    Circles details filtered depending on current user status
    if current user is the creator then highlight deadlines,
    also if current user is a member of the circle (creators assumed
    members by default) then link to submit remix if circle is in progress
    .
    
    Extracted out check for whether user is a member of a circle or not -
    many more OO refactor tidbits required.

--- app/models/circle.rb
+++ app/models/circle.rb
@@ -8,4 +8,10 @@ class Circle < ActiveRecord::Base
   validates_presence_of :submit_deadline
   validates_uniqueness_of :name, scope: :creator_id
   validates :signup_deadline, date: { before: :submit_deadline }
+
+  def has_member(user)
+    circle_members = Submission.where(circle_id: self.id)
+                                  .map { |submission| submission.original.creator }
+    circle_members.include? user
+  end
 end
