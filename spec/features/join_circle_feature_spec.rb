561b752f769e0b28d7aab4e3bb56517f7e6c0861
jonnyparris <ruskin.constant+dev@gmail.com>
2015-01-29 18:33:59 +0100

    refactor join circle feature spec with factory girl

--- spec/features/join_circle_feature_spec.rb
+++ spec/features/join_circle_feature_spec.rb
@@ -1,27 +1,15 @@
 feature "View, then join Circle" do
   before :each do
     User.delete_all
-    @j_dilla = User.create(first_name: Faker::Name.first_name,
-                           last_name: Faker::Name.last_name,
-                           user_name: Faker::Internet.user_name,
-                           email: "a",
-                           avatar: Faker::Avatar.image,
-                           location: Faker::Address.city,
-                           password: "poppop"
-                           )
+    @j_dilla = create(:user)
     visit login_path
-    fill_in "email", with: "a"
+    fill_in "email", with: @j_dilla.email
     fill_in "password", with: "poppop"
     click_button("Login")
     Track.delete_all
-    @new_beat = Track.create(download_url: "something",
-                             track_name: "BreezeBlock",
-                             creator_id: @j_dilla.id)
+    @new_beat = create(:track, creator: @j_dilla)
     Circle.delete_all
-    @xmas = Circle.create(name: "Xmas remixes",
-                          signup_deadline: Faker::Time.forward(23, :midnight),
-                          submit_deadline: Faker::Time.forward(300, :midnight),
-                          creator_id: @j_dilla.id)
+    @xmas = create(:circle, creator: @j_dilla)
   end
 
   scenario "should be successful if within signup deadline" do
@@ -33,16 +21,8 @@ feature "View, then join Circle" do
   end
 
   scenario "should have a list of circle members" do
-    @rick_rubin = User.create(first_name: Faker::Name.first_name,
-                              last_name: Faker::Name.last_name,
-                              user_name: Faker::Internet.user_name,
-                              email: "b",
-                              avatar: Faker::Avatar.image,
-                              location: Faker::Address.city,
-                              password: "poppop")
-    @newer_beat = Track.create(download_url: "something",
-                             track_name: "rock beat",
-                             creator_id: @rick_rubin.id)
+    @rick_rubin = create(:user)
+    @newer_beat = create(:track, creator: @rick_rubin)
     visit circle_path(@xmas)
     expect(page).to_not have_content(@rick_rubin.user_name)
     Submission.delete_all
