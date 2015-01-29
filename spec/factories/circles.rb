a72448979db163baf338a61b1dd81030b23b7624
jonnyparris <ruskin.constant+dev@gmail.com>
2015-01-29 18:57:30 +0100

    upgrade circles factory to include traits

--- spec/factories/circles.rb
+++ spec/factories/circles.rb
@@ -2,7 +2,24 @@ FactoryGirl.define do
   factory :circle do
     creator
     name Faker::Company.name
-    signup_deadline Faker::Time.forward(23, :midnight)
-    submit_deadline Faker::Time.forward(300, :midnight)
+
+    trait :future do
+      signup_deadline Faker::Time.forward(23, :midnight)
+      submit_deadline Faker::Time.forward(300, :midnight)
+    end
+
+    trait :present do
+      signup_deadline Faker::Time.backward(23, :midnight)
+      submit_deadline Faker::Time.forward(300, :midnight)
+    end
+
+    trait :past do
+      signup_deadline Faker::Time.backward(300, :midnight)
+      submit_deadline Faker::Time.backward(23, :midnight)
+    end
+
+    factory :future_circle,   traits: [:future]
+    factory :present_circle,   traits: [:present]
+    factory :past_circle,   traits: [:past]
   end
 end
