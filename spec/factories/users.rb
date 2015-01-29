b789c3101435a2ebd76e695487f2db38cba4b372
jonnyparris <ruskin.constant+dev@gmail.com>
2015-01-29 18:33:03 +0100

    make factory user username sequential

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,8 @@ FactoryGirl.define do
   factory :user, aliases: [:creator] do
     first_name Faker::Name.first_name
     last_name Faker::Name.last_name
-    user_name Faker::Internet.user_name
-    sequence(:email) { |n| "bewbs#{n}@email.com"}
+    sequence(:user_name) { |n| "#{Faker::Internet.user_name}#{n}" }
+    sequence(:email) { |n| "bewbs#{n}@email.com" }
     avatar Faker::Avatar.image
     location Faker::Address.city
     password "poppop"
