368633f2166355f5513e103f6c1fc3d3b0a3ad44
jonnyparris <ruskin.constant+dev@gmail.com>
2015-01-30 19:46:38 +0100

    create and pass mixup remix allocation result view test

--- spec/features/mixup_circle_feature_spec.rb
+++ spec/features/mixup_circle_feature_spec.rb
@@ -15,6 +15,17 @@ feature "Mixup Circle" do
     expect(page).to have_content("Start mixup")
   end
 
+  scenario "should declare which stem was allocated to current user for remix" do
+    present_circle = create(:present_circle)
+    create_list(:stem_submit, 5, circle: present_circle)
+    create(:submission, circle: present_circle, original: @new_beat)
+    visit circle_path(present_circle)
+    expect(page).to_not have_content("Waiting for")
+    present_circle.mixup
+    visit circle_path(present_circle)
+    expect(page).to have_content("Waiting for")
+  end
+
   xscenario "should trigger when signup deadline passes" do
 
   end
