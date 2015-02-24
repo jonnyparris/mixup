5ee9c9b1c5c48b4a561665c3fe84965d446911cc
jonnyparris <ruskin.constant+dev@gmail.com>
2015-02-24 16:06:29 +0100

    Enable Update and Destroy for circles
    new edit view
    message for circles created in the past by accident

--- app/controllers/circles_controller.rb
+++ app/controllers/circles_controller.rb
@@ -1,6 +1,7 @@
 class CirclesController < ApplicationController
   layout "users"
   before_action :current_user
+  before_action :find_circle, except: [:index, :new, :create, :show]
 
   def index
     @circles = Circle.includes(:creator, :submissions)
@@ -23,6 +24,36 @@ class CirclesController < ApplicationController
     end
   end
 
+  def edit
+    unless user_is_creator?
+      flash[:error] = ["Sorry, something went wrong. Please try again",
+                      "You didn't create this circle so you can't edit its details!"]
+      redirect_to circles_path
+    end
+  end
+
+  def update
+    if @circle.update_attributes(circle_params)
+      flash[:success] = "Sweet! Circle was successfully updated"
+      redirect_to circle_path(@circle)
+    else
+      flash[:error] = ["Sorry, something went wrong. Please try again",
+                      @circle.errors.full_messages.to_sentence]
+      redirect_to edit_circle_path(@circle)
+    end
+  end
+
+  def destroy
+    if @circle.destroy
+      flash[:success] = "Sweet! Circle was successfully deleted"
+      redirect_to user_dashboard_path(@current_user)
+    else
+      flash[:error] = ["Sorry, something went wrong. Please try again",
+                      @circle.errors.full_messages.to_sentence]
+      redirect_to circle_path(@circle)
+    end
+  end
+
   def show
     @circle = Circle.includes(:creator, :submissions).find(params[:id])
     @circle_members = @circle.members
@@ -32,9 +63,17 @@ class CirclesController < ApplicationController
 
   private
 
-  def circle_params
-    params.require(:circle).permit(:name,
-                                   :signup_deadline,
-                                   :submit_deadline)
-  end
+    def circle_params
+      params.require(:circle).permit(:name,
+                                     :signup_deadline,
+                                     :submit_deadline)
+    end
+
+    def find_circle
+      @circle = Circle.find(params[:id])
+    end
+
+    def user_is_creator?
+      current_user.id == find_circle.creator_id
+    end
 end
