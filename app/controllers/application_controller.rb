8693cd35813ecb0f952d5ad418c03c8d1f453c8a
jonnyparris <ruskin.constant+dev@gmail.com>
2015-02-24 12:58:25 +0100

    Bugfix: user signup works regardless of current session
    Extract setting sessions to application controller.
    Add login step to new user signup process so that redirect to
    dashboard works.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,12 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.includes(:circles,:stems,:remixes).find(session[:user_id]) if session[:user_id]
   end
+
+  def logout
+    session[:user_id] = nil
+  end
+
+  def login(user_id)
+    session[:user_id] = user_id
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
+      login(user.id)
       flash[:success] = "Welcome back!"
       redirect_to user_dashboard_path(user.id)
     else
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[:user_id] = nil
+    logout
     flash[:success] = "Logged out successfully. We'll miss you xx"
     redirect_to root_url
   end
