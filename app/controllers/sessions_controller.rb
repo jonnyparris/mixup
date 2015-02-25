4fe43c64c8872a576a0e137fc923d34ded995f32
jonnyparris <ruskin.constant+dev@gmail.com>
2015-02-25 18:39:44 +0100

    Add modals for login from homepage with indication of next step
    new next_step added to remember what user was trying to do before
    being prompted to login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,11 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       login(user.id)
       flash[:success] = "Welcome back!"
-      redirect_to user_dashboard_path(user.id)
+      if params[:next_step] == "create_circle"
+        redirect_to new_circle_path
+      else
+        redirect_to user_dashboard_path(user.id)
+      end
     else
       flash.now[:error] = ["Nah mate.","Your login failed."]
       render 'new'
