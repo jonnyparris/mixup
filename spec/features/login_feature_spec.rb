dbf815e4238fd1b6b0f8033c5d140f9a00873842
jonnyparris <ruskin.constant+dev@gmail.com>
2015-02-28 17:48:19 +0100

    add test for login helper to replace setting rack sessions directly

--- spec/features/login_feature_spec.rb
+++ spec/features/login_feature_spec.rb
@@ -1,17 +1,19 @@
 feature "Login" do
+  before :all do
+    @dilla = create(:user)
+  end
+
   scenario "should be successful with a welcome flash" do
-    dilla = create(:user)
     visit login_path
-    fill_in "email", with: dilla.email
+    fill_in "email", with: @dilla.email
     fill_in "password", with: "poppop"
     click_button("Login")
     expect(page).to have_content("Welcome")
   end
 
   scenario "should create a remember token if ticked" do
-    dilla = create(:user)
     visit login_path
-    fill_in "email", with: dilla.email
+    fill_in "email", with: @dilla.email
     fill_in "password", with: "poppop"
     check "Remember me"
     click_button("Login")
@@ -19,7 +21,13 @@ feature "Login" do
 
     expire_cookies
 
-    visit user_dashboard_path(dilla)
+    visit user_dashboard_path(@dilla)
+    expect(page).to have_content("Home")
+  end
+
+  scenario "should work with login_helper" do
+    login_as(@dilla)
+    visit user_dashboard_path(@dilla)
     expect(page).to have_content("Home")
   end
 end
