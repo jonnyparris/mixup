d02fd96f42fbf7e841a9e21420bd810856c3c1d2
jonnyparris <ruskin.constant+dev@gmail.com>
2015-04-20 16:34:12 +0200

    update bucket variable name

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,5 +4,5 @@ CarrierWave.configure do |config|
     :aws_access_key_id      => ENV["aws_access_key_rmx"],
     :aws_secret_access_key  => ENV["aws_access_token_rmx"]
   }
-  config.fog_directory  = ENV["aws_bucket"]
+  config.fog_directory  = ENV["aws_bucket_rmx"]
 end
